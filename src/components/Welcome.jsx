d43d904fe997a261fd7f8059a202729a4b595957
anirudh2843 <danirud4@gamil.com>
2025-04-17 14:26:15 +0530

    added backend url

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -15,16 +15,16 @@ const Welcome = () => {
     <div className="flex flex-col items-center justify-center min-h-screen text-5xl">
       <h1>
         Welcome to the{" "}
-        <span className="font-bold italic text-red-400">Coder's World!!!</span>
+        <span className="font-bold italic text-yellow-500">Coder's World!!!</span>
       </h1>
-      <h2 className="text-xl text-yellow-500 mt-4 italic">
-        Connect with{" "}
-        <span className="font-bold text-4xl text-green-400">
-          like-minded people
+      <h2 className="text-xl font-bold text-yellow-500 mt-4 italic">
+        Connect with{" "} 
+        <span className="font-bold text-4xl text-gray-200">
+           like-minded people
         </span>
       </h2>
       <button
-        className="border bg-red-100 text-black text-2xl italic px-2 py-2 rounded-2xl cursor-pointer mt-2"
+        className="border border-blue-500 bg-black text-white text-2xl italic px-2 py-2 rounded-2xl cursor-pointer mt-3"
         onClick={() => navigate("/login")}
       >
         Let's Start
