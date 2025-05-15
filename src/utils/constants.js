b83844f91d84982eb462494363a56c376249c895
anirudh2843 <danirud4@gamil.com>
2025-05-15 21:47:29 +0530

    add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://codemate-2.onrender.com"
-    : "http://localhost:10000";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:10000"
+    : "https://codemate-2.onrender.com";
